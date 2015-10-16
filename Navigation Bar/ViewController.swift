df4e54fa32a1c407dc281020a5720217015f4f40
Alicia Rodriguez <aliciarodriguez@alicias-mbp.att.net>
2015-10-15 23:06:02 -0400

    Initial Commit

---
+++ Navigation Bar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigation Bar
+//
+//  Created by Alicia Rodriguez on 10/15/15.
+//  Copyright © 2015 Alicia Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
